06580d5ad5d1415b7c7c05bebeecfb221e5aa0ac
Michał Giza <m.giza12345@gmail.com>
2024-10-24 15:23:57 +0200

    Dodaj walidację loginu

--- register.php
+++ register.php
@@ -3,5 +3,29 @@
 
     require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
 
-    
+    if(isset($_POST['login']))
+    {
+        $success = true;
+
+        //WALIDACJA LOGINU
+        $login = $_POST['login'];
+        if(strlen($login) < 4 || strlen($login) > 25){
+            $success = false;
+            $_SESSION['login_error'] = "Login musi mieć długość od 4 do 25 znaków!";
+        }
+        if(!ctype_alnum($login))
+        {
+            $success = false;
+            $_SESSION['login_error'] = "Login może składać się tylko z liter i cyfr!";
+        }
+        //Sprawdzenie czy istnieje już konto z takim loginem
+        $l_query = $db->prepare("SELECT user_id FROM user WHERE login = :login");
+        $l_query->bindValue(':login', $login, PDO::PARAM_STR); $l_query->execute();
+        $account = $l_query->fetch();
+        if($account)
+        {
+            $success = false;
+            $_SESSION['login_error'] = "Konto z podanym loginem już istnieje!";
+        }
+    }
 ?>
