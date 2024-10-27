1249cd904887d7a4e551bcd47a1e5adbc3deab4a
pawel <pawel.marcisz1@gmail.com>
2024-10-27 18:08:27 +0100

    Dodaj sprawdzanie hasła

--- login.php
+++ login.php
@@ -19,4 +19,17 @@
             $query->execute();
             $queryResult = $query->fetch();
         }
+
+        //Sprawdzanie hasła
+        if ($queryResult && password_verify($pass, $queryResult['password'])) {
+            $_SESSION['logged_id'] = $row['user_id'];
+            $_SESSION['logged_login'] = $row['login'];
+            $_SESSION['logged_username'] = $row['user_name'];
+            $_SESSION['logged_email'] = $row['email'];
+            unset($_SESSION['login_error']);
+            header('Location: index.php');
+        } else {
+            $_SESSION['login_error'] = "Podane dane logowania są nieprawidłowe!";
+            header('Location:'. $_SERVER['HTTP_REFERER']);
+        }
     }
