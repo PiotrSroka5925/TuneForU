a468599afaf1e134619d8f1a19acff909b59a1e0
Michał Wiewiórka <zolw25rudy@o2.pl>
2024-10-28 19:58:30 +0100

    Dodaj strukturę pliku html

--- register.php
+++ register.php
@@ -70,7 +70,19 @@
             $query->bindValue(':email', $email, PDO::PARAM_STR);
             $query->execute();
 
-            header('Location: '.$protocol.$_SERVER['HTTP_HOST'].'/gwork/index.php'); 
+            header('Location: '.$protocol.$_SERVER['HTTP_HOST'].'/tuneforu/index.php'); 
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
