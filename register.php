e02969c9d01d97e0eb5dba5a558968bc155f5ca6
Michał Wiewiórka <zolw25rudy@o2.pl>
2024-10-31 18:44:00 +0100

    Dodaj formularz rejestracji

--- register.php
+++ register.php
@@ -97,7 +97,7 @@
                     <h1 class="header">Twój Muzyczny Świat</h1> 
 
                     <form action="login.php" method="POST" class="loginForm pt-5">
-                        <input type="text" name="login" placeholder="Login">
+                        <input type="text" name="login" placeholder="Login lub e-mail">
                         <input type="password" name="password" placeholder="Hasło">
                         <input type="submit" value="Zaloguj się">
                     </form>
@@ -109,23 +109,28 @@
                         </button>
                     </div>
 
-                    <!--<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
+                            <div class="modal-content bg-black">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Rejestracja</h1>
+                                    <button type="button" class="ms-auto border border-0 bg-transparent text-white" data-bs-dismiss="modal" aria-label="Close"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                                        </svg>
+                                    </button>
                                 </div>
-                                <div class="modal-body">
-                                    
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Understood</button>
+                                <div class="modal-body d-flex flex-column align-items-center justify-content-center">
+                                    <form method="POST" class="loginForm">
+                                        <input type="text" name="login" placeholder="Login">
+                                        <input type="email" name="email" placeholder="E-mail">
+                                        <input type="password" name="password1" placeholder="Hasło">
+                                        <input type="password" name="password2" placeholder="Potwierdź Hasło">
+                                        <input type="submit" value="Zarejestruj się">
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                    </div>-->
+                    </div>
                 </div>
             </div>
         </div>
