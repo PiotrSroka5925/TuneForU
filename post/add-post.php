de7e2c600cfa158bfa4a7da9bc2f13f946484aba
Michał Giza <m.giza12345@gmail.com>
2024-11-02 16:23:28 +0100

    Dodaj walidację rozmiaru plików

--- post/add-post.php
+++ post/add-post.php
@@ -3,7 +3,7 @@
 
     require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
 
-    if(isset($_SESSION['logged_id']) && isset($_POST['title'])){
+    if(!isset($_SESSION['logged_id']) && isset($_POST['title'])){
         $success = true;
 
         $title = $_POST['title'];
@@ -17,5 +17,13 @@
             $success = false;
             $_SESSION['text_error'] = "Treść musi mieć od 3 do 500 znaków!";
         }
+
+        if (!empty($_FILES['upload']['name'][0])) {
+            if(array_sum($_FILES['upload']['size']) / 1048576 > 10){
+                $success = false;
+                $_SESSION['data_error'] = "Maksymalny rozmiar plików to 10MB";
+            }
+        }
     }
 ?>
+
