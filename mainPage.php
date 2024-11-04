4038d6e52eef81bfe5bf6312ee2ddd0705006358
Michał Wiewiórka <zolw25rudy@o2.pl>
2024-11-04 21:41:23 +0100

    Utwórz plik styli do strony głównej i dołącz do pliku strony głównej

--- mainPage.php
+++ mainPage.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TuneForU</title>
+    <link rel="stylesheet" href="styles/styleMainPage.css">
 </head>
 <body>
     
