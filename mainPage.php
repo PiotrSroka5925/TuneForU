d051ab05f7f2b386d5456cdf7c9fdcea0366e8a5
Michał Wiewiórka <zolw25rudy@o2.pl>
2024-11-04 23:44:58 +0100

    Dodaj podstawową strukturę strony głównej

--- mainPage.php
+++ mainPage.php
@@ -7,8 +7,26 @@
     <link rel="stylesheet" href="styles/styleMainPage.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    
+<body class="bg-black">
+    <div class="container d-flex">
+        <div class="col menu">
+            <img src="img/logo_white.png" alt="logo">
+            <div>
+                <ul>
+                    <a href="#"><li>home</li></a>
+                    <a href="#"><li>following</li></a>
+                    <a href="#"><li>notifications</li></a>
+                    <a href="#"><li>profile</li></a>
+                </ul>
+            </div>
+        </div>
+        <div class="col">
+            posts
+        </div>
+        <div class="col">
+            additional content
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
