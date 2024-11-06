007814bafde66f59adf91bea2e1b34f48e295a55
Michał Giza <michalgiza8b@gmail.com>
2024-11-06 20:25:06 +0100

    Dodaj zmienną redirectUrl przechowującą adres do przekierowań

--- post/post.php
+++ post/post.php
@@ -3,6 +3,8 @@
 
     require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
 
+    $redirectUrl = $protocol.$_SERVER['HTTP_HOST'].'/tuneforu/index.php';
+
     if(isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT)){
         $query = $db->prepare("SELECT * FROM post JOIN user USING (user_id) WHERE post_id = :post_id");
         $query->bindValue(':post_id', $_GET['id'], PDO::PARAM_INT);
@@ -11,6 +13,6 @@
         $post = $query->fetch();
     }
     else{
-        header('Location: '.$protocol.$_SERVER['HTTP_HOST'].'/tuneforu/index.php'); 
+        header('Location: '.$redirectUrl); 
     }
 ?>
