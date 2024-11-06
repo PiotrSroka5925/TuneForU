710090ddbb86221a66ba8ef3226beb4d0af44484
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-11-06 21:27:56 +0100

    Usuń niepotrzebne elementy nawigacji

--- navigation.php
+++ navigation.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="styles/styleNavigationPage.css">
-</head>
-<body>
-<div class="sidebar">
-<img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white_upscaled.png"?>">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<div class="sidebar bg-black" style="width: 200px;">
+    <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white_upscaled.png"?>" class="img-fluid">
     <ul class="nav flex-column">
         <li class="nav-item">
             <a class="nav-link" href="#">
@@ -24,7 +17,7 @@
         <li class="nav-item">
             <a class="nav-link" href="#">
                 <i class="bi bi-bell-fill"></i> Notifications
-            </a>
+            </a> 
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
@@ -42,7 +35,5 @@
             </a>
         </li>
     </ul>
-    <button class="btn btn-custom mt-4">Post</button>
+    <button class="btn btn-custom mt-4 text-white">Post</button>
 </div>
-</body>
-</html>
