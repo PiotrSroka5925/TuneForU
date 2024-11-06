16526a3c799f7620dcb4e9bd0a3a580d119056aa
Michał Giza <michalgiza8b@gmail.com>
2024-11-06 20:18:52 +0100

    Dodaj zapytanie pobierające dane o wpisie i użytkowniku z bazy danych z id podanym w adresie URL

--- post/post.php
+++ post/post.php
@@ -4,7 +4,11 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
 
     if(isset($_GET['id'])){
+        $query = $db->prepare("SELECT * FROM post JOIN user USING (user_id) WHERE post_id = :post_id");
+        $query->bindValue(':post_id', $_GET['id'], PDO::PARAM_INT);
+        $query->execute();
 
+        $post = $query->fetch();
     }
     else{
         header('Location: '.$protocol.$_SERVER['HTTP_HOST'].'/tuneforu/index.php'); 
