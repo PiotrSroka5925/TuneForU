176ff48ab66aae5ccf704a6ad6c8d212a7f6470a
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-11-06 21:55:43 +0100

    Zmień odniśniki w nawigacji

--- navigation.php
+++ navigation.php
@@ -1,8 +1,15 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
+
+?>
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<div class="sidebar bg-black" style="width: 200px;">
-    <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white_upscaled.png"?>" class="img-fluid">
+<link rel="stylesheet" href="styles/styleNavigationPage.css">
+<div class="sidebar bg-black" style="width: 200px;">    
+    <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white_upscaled.png"?>" class="img-fluid d-none d-md-block" id="upscaledLogo">            
+    <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white.png"?>" class="img-fluid d-md-none d-sm-block">
     <ul class="nav flex-column">
         <li class="nav-item">
             <a class="nav-link" href="#">
@@ -11,7 +18,7 @@
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-                <i class="bi bi-search"></i> Explore
+                <i class="bi bi-search"></i> Following
             </a>
         </li>
         <li class="nav-item">
@@ -21,19 +28,11 @@
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-                <i class="bi bi-envelope-fill"></i> Messages
+                <i class="bi bi-envelope-fill"></i> Profile
             </a>
-        </li>
+        </li>    
         <li class="nav-item">
-            <a class="nav-link" href="#">
-                <i class="bi bi-bookmark-fill"></i> Bookmarks
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-                <i class="bi bi-person-fill"></i> Profile
-            </a>
-        </li>
-    </ul>
-    <button class="btn btn-custom mt-4 text-white">Post</button>
+            <button class="btn btn-custom mt-2 ms-1 text-white">Post</button>
+        </li>   
+    </ul>    
 </div>
