e1335174a54fa6a504eff7662987e502114b5c43
Michał Giza <michalgiza8b@gmail.com>
2024-11-06 20:15:27 +0100

    Dodaj przekierowanie, gdy nie ma parametru id w adresie URL

--- post/post.php
+++ post/post.php
@@ -2,4 +2,12 @@
     session_start();
 
     require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
+
+    if(isset($_GET['id'])){
+
+    }
+    else{
+        header('Location: '.$protocol.$_SERVER['HTTP_HOST'].'/tuneforu/index.php'); 
+    }
+
 ?>
