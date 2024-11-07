316a8b6417e33b4f2fee87ff6b32323075055d75
Michał Wiewiórka <zolw25rudy@o2.pl>
2024-11-07 23:59:41 +0100

    Dodaj link do boostrap icons

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>TuneForU</title>
     <link rel="stylesheet" href="styles/styleMainPage.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
 <body class="bg-black">
     <div class="container d-flex">
@@ -64,11 +65,12 @@
                 </div>
 
                 <div class="row d-flex postImages mt-3">
-
+                    <img src="#" alt="post_image">
+                    <img src="#" alt="post_image">
+                    <img src="#" alt="post_image">
                 </div>
 
-                <div class="row postInteractions">
-
+                <div class="row d-flex justify-content-between mt-3 postInteractions">
                 </div>
             </div>
         </div>
