c453a4914e79d47bc896e0f0dd8ad9a094223273
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-11-07 22:01:01 +0100

    Dodaj pobieranie i wyświetlanie danych użytkownika

--- navigation.php
+++ navigation.php
@@ -1,5 +1,18 @@
 <?php
+
+session_start();
+unset($_SESSION['logged_id']);
 require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
+
+
+if(isset($_SESSION['logged_id'])){
+                
+    $query = $db->prepare("SELECT * FROM user WHERE user_id = :user_id");
+    $query->bindParam(':user_id', $_SESSION['logged_id'], PDO::PARAM_INT);
+    $query->execute();
+    $user = $query->fetch();            
+}
+  
 
 ?>
 
@@ -8,9 +21,8 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="styles/styleNavigationPage.css">
 <div class="sidebar bg-black" style="width: 200px;">    
-    <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white_upscaled.png"?>" class="img-fluid d-none d-md-block" id="upscaledLogo">            
-    <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white.png"?>" class="img-fluid d-md-none d-sm-block">
-    <ul class="nav flex-column">
+    <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white_upscaled.png"?>" class="img-fluid d-none d-md-block" id="upscaledLogo">                
+    <ul class="nav flex-column p-2">
         <li class="nav-item">
             <a class="nav-link" href="#">
                 <i class="bi bi-house-door-fill"></i> Home
@@ -26,13 +38,19 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
                 <i class="bi bi-bell-fill"></i> Notifications
             </a> 
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-                <i class="bi bi-envelope-fill"></i> Profile
-            </a>
+        <li class="nav-item> <?php if(!isset($_SESSION['logged_id'])) echo "d-none" ?>">
+            <div class="d-flex">                
+                <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu ".$user['profile_picture']?>" class="img-fluid rounded-circle" width="50" height="50" alt="">
+                <div class="d-flex flex-column">
+                    <span><?=$user['user_name']?></span>
+                    <span>@<?=$user['login']?></span>
+                </div>
+            </div>                       
         </li>    
         <li class="nav-item">
-            <button class="btn btn-custom mt-2 ms-1 text-white">Post</button>
+            <button class="btn btn-custom mt-2 ms-1 text-white <?php if(!isset($_SESSION['logged_id'])) echo "disabled"
+            ?>" data-bs-toggle="modal">Post</button>
         </li>   
-    </ul>    
+    </ul>   
+    
 </div>
