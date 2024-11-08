ae81502da4a08ba51fa2bbbc14da2172c31f5c85
Michał Giza <michalgiza8b@gmail.com>
2024-11-08 17:04:57 +0100

    Dodaj obliczenie przesunięcia dla paginacji oparte na zakresie podanym w atrybucie $_POST['range']

--- post/get-posts.php
+++ post/get-posts.php
@@ -1,4 +1,15 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
 
+    if(isset($_POST['range'])){
+        $range = filter_input(INPUT_POST, 'range');
+
+        $range_length = 5;
+
+        if ($range && $range > 0) {
+            $limit_min = ($range - 1) * $range_length;
+        } else {
+            $limit_min = 0;
+        }
+    }
 ?>
