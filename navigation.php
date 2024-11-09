8274d038483ac1b774def6c467cbc5fe546551a5
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-11-09 01:50:12 +0100

    Zmień  ułożenie odnośników

--- navigation.php
+++ navigation.php
@@ -20,29 +20,33 @@ if(isset($_SESSION['logged_id'])){
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="styles/styleNavigationPage.css">
+
 <div class="sidebar bg-black">    
     <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/img/logo_white_upscaled.png"?>" class="img-fluid d-none d-md-block" width="200">                
     <ul class="nav flex-column p-2">
         <li class="nav-item">
             <a class="nav-link" href="#">
-                <i class="bi bi-house-door-fill"></i> Główna
+                <i class="bi bi-house-door-fill"></i> 
+                <span class="d-none d-sm-inline">Główna</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-                <i class="bi bi-search"></i> Przeglądaj
+                <i class="bi bi-search"></i> 
+                <span class="d-none d-sm-inline">Przeglądaj</span>
             </a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">
-                <i class="bi bi-bell-fill"></i> Powiadomienia
+                <i class="bi bi-bell-fill"></i> 
+                <span class="d-none d-sm-inline">Powiadomienia</span>
             </a> 
         </li>
         <li class="nav-item m-0 bg-black <?php if(!isset($_SESSION['logged_id'])) echo "d-none" ?>">            
             <div class="dropdown d-flex">
-                <button class="btn btn-profile text-white d-flex align-items-center border border-0 mb-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="btn btn-profile text-white d-flex align-items-center border border-0 mb-3" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu ".$user['profile_picture']?>" class="img-fluid rounded-circle me-2" width="50" height="50" alt="">
-                    <div class="d-flex flex-column text-start">
+                    <div class="d-none d-sm-flex flex-column text-start">
                         <span><?=$user['user_name']?></span>
                         <span>@<?=$user['login']?></span>
                     </div>
@@ -53,7 +57,6 @@ if(isset($_SESSION['logged_id'])){
                             Profil
                         </a>
                     </li>                                      
-                                      
                     <li>
                         <a class="dropdown-item dropdown-item-bg text-light " href="logout.php">
                             Wyloguj się
@@ -66,15 +69,17 @@ if(isset($_SESSION['logged_id'])){
         echo '
         <li class="nav-item">
             <a href="register.php">
-                <button class="btn btn-custom text-white ms-3 fw-bold align-items-center" data-bs-toggle="modal"><i class="bi bi-person-fill"></i> Zaloguj się</button>
+                <button class="btn btn-custom text-white ms-3 fw-bold align-items-center" data-bs-toggle="modal"><i class="bi bi-person-fill navIcon"></i> <span class="navWriting">Zaloguj się</span></button>
             </a>
         </li>   
         ';        
         ?>  
         <li class="nav-item">
-            <button class="btn mt-2 ms-3 text-white btn-custom fw-bold <?php if(!isset($_SESSION['logged_id'])) echo "d-none"?>" data-bs-toggle="modal">Post</button>
+            <button class="btn btn-custom text-white ms-3 fw-bold align-items-center <?php if(!isset($_SESSION['logged_id'])) echo "d-none"?>" data-bs-toggle="modal">
+                <i class="bi bi-pencil-fill navIcon"></i>
+                <span class="navWriting">Post</span>
+            </button>
         </li>         
                 
     </ul>   
-    
 </div>
