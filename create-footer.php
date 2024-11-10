3ae142e9e8165b2607e52a75ef2731c926db6478
pawel <pawel.marcisz1@gmail.com>
2024-11-10 16:18:30 +0100

    Zmień teksty na polski

--- create-footer.php
+++ create-footer.php
@@ -20,17 +20,17 @@
                 <div class="col-lg-2 mb-4">
                     <h5 class="fw-semibold">Menu</h5>
                     <ul class="list-unstyled">
-                        <li class="mb-1"><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Home</a></li>
-                        <li class="mb-1"><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Search</a></li>
-                        <li class="mb-1"><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Profile</a></li>
-                        <li><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Contact</a></li>
+                        <li class="mb-1"><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Strona główna</a></li>
+                        <li class="mb-1"><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Szukaj</a></li>
+                        <li class="mb-1"><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Profil</a></li>
+                        <li><a href="#!" class="footerLink text-white text-decoration-none text-lightgray">Kontakt</a></li>
                     </ul>
                 </div>
 
                 <div class="col-lg-2 mb-4">
-                    <h5 class="fw-semibold">Contact</h5>
+                    <h5 class="fw-semibold">Kontakt</h5>
                     <ul class="list-unstyled text-lightgray">
-                        <li class="d-flex align-items-center mb-2"><i class="bi bi-geo-alt-fill fs-4 me-3"></i><p class="m-0">Adress</p></li>
+                        <li class="d-flex align-items-center mb-2"><i class="bi bi-geo-alt-fill fs-4 me-3"></i><p class="m-0">Adres</p></li>
                         <li class="d-flex align-items-center mb-2"><i class="bi bi-envelope-at-fill fs-4 me-3"></i><a class="m-0 text-decoration-none text-lightgray footerLink" href="mailto:email@example.com">email@example.com</a></li>
                         <li class="d-flex align-items-center"><i class="bi bi-telephone-fill fs-4 me-3"></i><p class="m-0">+48 123 456 789</p></li>
                     </ul>
