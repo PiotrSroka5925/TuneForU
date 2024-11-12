bd15f87746404f1eb80909daf9f2fa39f4eda2b2
Michał Giza <michalgiza8b@gmail.com>
2024-11-12 18:49:08 +0100

    Dodaj wyświetlanie danych użytkownika

--- post/post.php
+++ post/post.php
@@ -31,7 +31,29 @@
     <title>TuneForU</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-black text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+
+            </div>
+            <div class="col-6">
+                <div class="p-3 border border-secondary">
+                    <div class="d-flex w-100 align-items-center border-bottom border-secondary pb-2">
+                        <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu".$post['profile_picture']?>" alt="profile_picture" class="rounded-circle" width="55">
+                        <div class="w-100 d-flex align-items-center">
+                            <span class="ms-2 fs-4 fw-bold text-break"><?=$post['user_name']?></span>
+                            <span class="text-secondary align-self-end mb-1 text-break" style="font-size: 12px;"> @<?=$post['login']?></span>
+                            <span class="d-block ms-auto"> <?=$post['date']?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+
+            </div>
+        </div>
+    </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
