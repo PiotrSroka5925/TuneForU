e1896046ce5d919d8e6e622377be694fe77883a6
Michał Giza <michalgiza8b@gmail.com>
2024-11-12 19:59:59 +0100

    Dodaj menu i stopkę strony

--- post/post.php
+++ post/post.php
@@ -30,17 +30,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TuneForU</title>
     <link rel="stylesheet" href="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/styles/fullPage.css"?>">
+    <link rel="stylesheet" href="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/styles/styleNavigationPage.css"?>">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="bg-black text-white">
     <div id="fullpage"></div>
     <div class="container">
         <div class="row">
-            <div class="col-3">
-                sda
+            <div class="col-2">
+                <?php require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/navigation.php'); ?>
             </div>
-            <div class="col-9 col-md-6">
-                <div class="px-3 py-2 border border-secondary">
+            <div class="col-10 col-md-7">
+                <div class="px-3 py-2 border-start border-secondary">
                     <div class="d-flex w-100 align-items-center border-bottom border-secondary pb-2">
                         <img src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu".$post['profile_picture']?>" alt="profile_picture" class="rounded-circle" width="55">
                         <div class="w-100 d-flex align-items-center">
@@ -79,6 +80,8 @@
             </div>
         </div>
     </div>
+
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/create-footer.php');?>
     
     <script src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/js/full-page-photo.js"?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
