7e9841c1915bb36092f0c964e3750a22ba6d2fa1
Michał Wiewiórka <zolw25rudy@o2.pl>
2024-11-14 00:37:15 +0100

    Dodaj skrypt obsługujący wyświetlanie zdjęcia z wpisu w pełnej rozdzielczości

--- user/user.php
+++ user/user.php
@@ -36,6 +36,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../styles/styleNavigationPage.css"> 
+    <link rel="stylesheet" href="../styles/fullPage.css">
 </head>
 <body class="bg-black text-white">
     <div id="fullpage"></div>
@@ -61,7 +62,7 @@
                             </div>
                         </div>
                     </div>
-                        
+                       
                     <select name="order" id="orderSelect" class="form-select mt-5" style="width: 50%;">
                             <option value="date" selected>Data dodania</option>
                             <option value="popularity7Days">Popularne - 7 dni</option>
@@ -89,6 +90,7 @@
     <script src="js/preview-images.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="../js/get-user-posts.js"></script>
-    <script src="../js//like-post.js"></script>
+    <script src="../js/like-post.js"></script>
+    <script src="../js/full-page-photo.js"></script>
 </body>
 </html>
