e7dd000afc7e1439a904b6abbdaf1638195ed749
Michał Giza <michalgiza8b@gmail.com>
2024-11-14 20:07:07 +0100

    Dodaj stopki do stron

--- register.php
+++ register.php
@@ -243,6 +243,9 @@
         </div>
     </div>
 
+    <?php 
+        require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/create-footer.php');
+    ?>
     
     <script src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/js/register-profile-picture.js"?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- user/edit-user-form.php
+++ user/edit-user-form.php
@@ -128,6 +128,9 @@
 
     </div>
 
+    <?php 
+        require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/create-footer.php');
+    ?>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="<?=$protocol.$_SERVER['HTTP_HOST']."/tuneforu/js/register-profile-picture.js"?>"></script>
