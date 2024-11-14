3860cd5485c7b5412ded65886e4f4225f093fdfc
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-11-14 02:21:47 +0100

    Dodaj wyglad formularza edycji profilu

--- navigation.php
+++ navigation.php
@@ -66,8 +66,8 @@
             </button>
             <div class="dropdown align-items-center d-flex">                
                 <ul class="dropdown-menu dropdown-menu-blur w-100 m-0 rounded-4" aria-labelledby="userDropdown">                   
-                    <li>
-                        <a class="dropdown-item dropdown-item-bg text-light " href="profile.php">
+                    <li>                    
+                        <a class="dropdown-item dropdown-item-bg text-light " href="">
                             Profil
                         </a>
                     </li>                                      
