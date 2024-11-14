0bc92cbb7de9129ff7656141e5175da435d3b5bf
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-11-14 17:21:04 +0100

    Usuń nieporzebną walidację loginu oraz sprawdź czy login z $_SESSION jest sam jak  z $_POST

--- edit-profile.php
+++ edit-profile.php
@@ -2,37 +2,26 @@
 session_start();
 require_once($_SERVER['DOCUMENT_ROOT'].'/tuneforu/database.php');
 
-if (isset($_POST['user_id'])) {
+if (isset($_POST['user_id']) && isset($_SESSION['logged_id'])) {
     $allValid = true;
     $user_id = $_POST['user_id'];
-    
-    
-    $login = $_POST['login'];
-    if (strlen($login) < 4 || strlen($login) > 25) {
-        $allValid = false;
-        $_SESSION['login_error'] = "Login musi mieć długość od 4 do 25 znaków!";
-    }
-    if (!ctype_alnum($login)) {
-        $allValid = false;
-        $_SESSION['login_error'] = "Login może składać się tylko z liter i cyfr!";
-    }
-   
-    $l_query = $db->prepare("SELECT user_id FROM user WHERE login = :login AND user_id != :user_id");
-    $l_query->bindValue(':login', $login, PDO::PARAM_STR);
-    $l_query->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-    $l_query->execute();
-    if ($l_query->fetch()) {
-        $allValid = false;
-        $_SESSION['login_error'] = "Konto z podanym loginem już istnieje!";
+
+    if ($_SESSION['logged_id'] != $user_id) {
+        $_SESSION['general_error'] = "Nie masz uprawnień do edycji tego profilu.";
+        header("Location: /tuneforu/index.php");
+        exit;
     }
 
- 
+    $query = $db->prepare("SELECT login FROM user WHERE user_id = :user_id");
+    $query->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $query->execute();
+    $login = $query->fetchColumn();
+
     $user_name = $_POST['user_name'];
     if (strlen($user_name) < 3 || strlen($user_name) > 25) {
         $allValid = false;
         $_SESSION['user_name_error'] = "Nazwa użytkownika musi mieć od 3 do 25 znaków!";
     }
-
 
     $email = $_POST['email'];
     $snt_email = filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -49,14 +38,12 @@ if (isset($_POST['user_id'])) {
         $_SESSION['email_error'] = "Konto z podanym adresem email już istnieje!";
     }
 
-  
     $query = $db->prepare("SELECT profile_picture FROM user WHERE user_id = :user_id");
     $query->bindValue(':user_id', $user_id, PDO::PARAM_INT);
     $query->execute();
     $current_picture = $query->fetchColumn();
     $profile_picture = $current_picture;
 
-  
     if (isset($_FILES['profile_picture']) && $_FILES['profile_picture']['size'] > 0) {
         if ($_FILES['profile_picture']['size'] / 1048576 > 2) { 
             $allValid = false;
@@ -80,30 +67,29 @@ if (isset($_POST['user_id'])) {
             }
         }
     }
-    
-    if (!$allValid) {
-        $_SESSION['old_login'] = $login;
-        $_SESSION['old_user_name'] = $user_name;
-        $_SESSION['old_email'] = $email;
-        $_SESSION['old_profile_picture'] = $profile_picture;
-        header("Location: /tuneforu/user/edit-user-form.php?error=true&id=" . urlencode($user_id));
-        exit();
-    }
-   
-    $query = "UPDATE user SET login = :login, user_name = :user_name, email = :email, profile_picture = :profile_picture WHERE user_id = :user_id";
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":login", $login, PDO::PARAM_STR);
-    $stmt->bindValue(":user_name", $user_name, PDO::PARAM_STR);
-    $stmt->bindValue(":email", $email, PDO::PARAM_STR);
-    $stmt->bindValue(":profile_picture", $profile_picture, PDO::PARAM_STR);
-    $stmt->bindValue(":user_id", $user_id, PDO::PARAM_INT);
 
-    if ($stmt->execute()) {
+    if ($allValid) {
+        $query = "UPDATE user SET user_name = :user_name, email = :email, profile_picture = :profile_picture WHERE user_id = :user_id";
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(":user_name", $user_name, PDO::PARAM_STR);
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        $stmt->bindValue(":profile_picture", $profile_picture, PDO::PARAM_STR);
+        $stmt->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            header("Location: /tuneforu/index.php");
+            exit();
+        } else {
+            $_SESSION['general_error'] = "Wystąpił błąd przy aktualizacji danych.";
+            header("Location: /tuneforu/user/edit-user-form.php?error=true&id=" . urlencode($user_id));
+            exit();
+        }
+    } else {
         header("Location: /tuneforu/user/edit-user-form.php?error=true&id=" . urlencode($user_id));
         exit();
-    } else {
-        $_SESSION['general_error'] = "Wystąpił błąd przy aktualizacji danych.";
-        header("Location: /tuneforu/user/edit-user-form.phpp?error=true&id=" . urlencode($user_id));
-        exit();
     }
+} else {
+    $_SESSION['general_error'] = "Niepoprawny dostęp.";
+    header("Location: /tuneforu/index.php");
+    exit();
 }
